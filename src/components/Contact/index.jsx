ba061c4adbd2384f7c8316beb0f0e890426ae702
Francisco <frandeluque@AirdeFrancisco.technicolor.net>
2022-07-22 10:56:50 +0200

    first commit

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from 'react'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+    emailjs
+      .sendForm(
+        'service_l1x6a7h',
+        'template_g1nzqoh',
+        form.current,
+        'xhEpwc113UKga91mJ'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          e.target.reset()
+          alert('Ready to go')
+        },
+        (error) => {
+          console.log(error.text)
+          alert('Something went wrong')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    placeholder="Name"
+                    type="text"
+                    name="name"
+                    id="name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="Send" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Contact
